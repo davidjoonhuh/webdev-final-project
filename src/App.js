0eb932f701272c1a70474b26fe203065e9a570f9
davidjoonhuh <davidjo.huh@gmail.com>
2023-08-17 19:15:22 -0400

    implemented nav bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import HelloWorld from './web-dev-project/hello-world';
-import SearchPage from './web-dev-project/search';
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router";
+import Youboxd from './web-dev-project';
+import SearchPage from './web-dev-project/search';
 
 function App() {
     return (
         <BrowserRouter>
             <div className="container">
                 <Routes>
-                    <Route path="/" element={<Navigate to="/hello" />} />
-                    <Route path="/hello" element={<HelloWorld />} />
-                    <Route path="/search" element={<SearchPage />} />
+                    <Route path="/" element={<Navigate to="/youboxd" />} />
+                    <Route path="/youboxd/*" element={<Youboxd />} />
+
                 </Routes>
             </div>
         </BrowserRouter>
---
+++ src/web-dev-project/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Route } from "react-router";
+import NavigationSidebar from "./navigation-sidebar";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
+import videoReducer from "./reducers/video-reducer";
+import SearchPage from "./search/search-bar";
+
+const store = configureStore(
+    { reducer: { vids: videoReducer } });
+
+function Youboxd() {
+    return (
+        <Provider store={store}>
+
+            <div>
+                <div className="row">
+                    <div className="col-2">
+                        <NavigationSidebar />
+                    </div>
+                    <div className="col-7">
+                        <Routes>
+                            <Route path="/search" element={<SearchPage/>} />
+                        </Routes>
+                    </div>
+                    <div className="col-3">
+                    </div>
+                </div>
+            </div>
+
+
+        </Provider>
+    );
+}
+export default Youboxd;
