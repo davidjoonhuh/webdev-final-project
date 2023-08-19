bb825808d9fc31b4bfd9d83fa0b7db7b0a38f132
Jinhyun Hwang <jinhyunh0808@gmail.com>
2023-08-19 16:56:29 -0400

    Added env with localhost as backend

--- src/web-dev-project/user/profile-screen.js
+++ src/web-dev-project/user/profile-screen.js
@@ -2,83 +2,83 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import {
-  profileThunk,
-  logoutThunk,
-  updateUserThunk,
+    profileThunk,
+    logoutThunk,
+    updateUserThunk,
 } from "../services/auth-thunks";
 
 function ProfileScreen() {
-  const { currentUser } = useSelector((state) => state.user);
-  const [profile, setProfile] = useState(currentUser);
+    const { currentUser } = useSelector((state) => state.user);
+    const [profile, setProfile] = useState(currentUser);
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  const save = async () => {
-    const action = await dispatch(updateUserThunk(profile));
+    const save = async () => {
+        const action = await dispatch(updateUserThunk(profile));
 
-    setProfile(action.payload);
-  };
-  const loadProfile = async () => {
-    const action = await dispatch(profileThunk());
+        setProfile(action.payload);
+    };
+    const loadProfile = async () => {
+        const action = await dispatch(profileThunk());
 
-    setProfile(action.payload);
-  };
+        setProfile(action.payload);
+    };
 
-  console.log(currentUser);
+    console.log(currentUser);
 
-  useEffect(() => {
-    loadProfile();
-  }, []);
+    useEffect(() => {
+        loadProfile();
+    }, []);
 
-  console.log(profile);
+    console.log(profile);
 
-  return (
-    <div>
-      <h1>Profile Screen</h1>
-      {profile && (
+    return (
         <div>
-          <div>
-            <label>First Name</label>
-            <input
-              type="text"
-              value={profile.firstName}
-              onChange={(event) => {
-                const newProfile = {
-                  ...profile,
-                  firstName: event.target.value,
-                };
-                setProfile(newProfile);
-              }}
-            />
-          </div>
-          <div>
-            <label>Last Name</label>
-            <input
-              type="text"
-              value={profile.lastName}
-              onChange={(event) => {
-                const newProfile = {
-                  ...profile,
-                  lastName: event.target.value,
-                };
-                setProfile(newProfile);
-              }}
-            />
-          </div>
+            <h1>Profile Screen</h1>
+            {profile && (
+                <div>
+                    <div>
+                        <label>First Name</label>
+                        <input
+                            type="text"
+                            value={profile.firstName}
+                            onChange={(event) => {
+                                const newProfile = {
+                                    ...profile,
+                                    firstName: event.target.value,
+                                };
+                                setProfile(newProfile);
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <label>Last Name</label>
+                        <input
+                            type="text"
+                            value={profile.lastName}
+                            onChange={(event) => {
+                                const newProfile = {
+                                    ...profile,
+                                    lastName: event.target.value,
+                                };
+                                setProfile(newProfile);
+                            }}
+                        />
+                    </div>
+                </div>
+            )}
+            <button
+                onClick={() => {
+                    dispatch(logoutThunk());
+                    navigate("/youboxd/login");
+                }}
+            >
+                {" "}
+                Logout
+            </button>
+            <button onClick={save}>Save </button>
         </div>
-      )}
-      <button
-        onClick={() => {
-          dispatch(logoutThunk());
-          navigate("/youboxd/login");
-        }}
-      >
-        {" "}
-        Logout
-      </button>
-      <button onClick={save}>Save </button>
-    </div>
-  );
+    );
 }
 export default ProfileScreen;
