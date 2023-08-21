53266831a156e5f8b6846919d4f901617f0dc821
jwzhou1 <115595414+jwzhou1@users.noreply.github.com>
2023-08-21 20:24:47 +0800

    Update for profile page

---
+++ src/web-dev-project/reducers/who-reducer.js
@@ -0,0 +1,34 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {
+  findWhoToFollowListThunk,
+} from "../services/who-thunks";
+
+const initialState = {
+  whoToFollowList: [],
+  loading: false
+}
+
+const whoToFollowListSlice = createSlice({
+  name: 'whoToFollowList',
+  initialState,
+  extraReducers: {
+    [findWhoToFollowListThunk.pending]:
+        (state) => {
+          state.loading = true
+          state.whoToFollowList = []
+        },
+    [findWhoToFollowListThunk.fulfilled]:
+        (state, {payload}) => {
+          state.loading = false
+          state.whoToFollowList = payload
+        },
+    [findWhoToFollowListThunk.rejected]:
+        (state, action) => {
+          state.loading = false
+          state.whoToFollowList = action.error
+        }
+  },
+  reducers: {}
+});
+
+export default whoToFollowListSlice.reducer;
