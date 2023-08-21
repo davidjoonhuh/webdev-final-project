c7dbbc6a44f46feba1ba21d80d5d81fb07d38a53
jwzhou1 <115595414+jwzhou1@users.noreply.github.com>
2023-08-21 20:48:17 +0800

    This is for following users

---
+++ src/web-dev-project/who-to-follow-list/index.js
@@ -0,0 +1,32 @@
+import React, {useEffect} from "react";
+import WhoToFollowListItem from "./who-to-follow-list-item";
+import {useDispatch, useSelector} from "react-redux";
+import {findWhoToFollowListThunk} from "../services/who-thunks";
+
+const WhoToFollowList = () => {
+  const { whoToFollowList, loading } = useSelector(state => state.whoToFollowList);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(findWhoToFollowListThunk())
+  }, [dispatch])
+  return (
+      <ul className="list-group">
+        <li className="list-group-item">
+          <h3>Who to follow</h3>
+        </li>
+        {loading &&
+            <li className="list-group-item">
+              Loading...
+            </li>
+        }
+        {
+          whoToFollowList.map(whoToFollow =>
+              <WhoToFollowListItem
+                  key={whoToFollow._id}
+                  whoToFollow={whoToFollow} />
+          )
+        }
+      </ul>
+  );
+};
+export default WhoToFollowList;
