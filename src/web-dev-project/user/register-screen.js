6ff3d84f65854dcdf75922123db27b96a3dff9c8
jwzhou1 <115595414+jwzhou1@users.noreply.github.com>
2023-08-22 07:48:30 +0800

    Update register-screen.js to have more register feature as required

--- src/web-dev-project/user/register-screen.js
+++ src/web-dev-project/user/register-screen.js
@@ -2,17 +2,21 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
 import { registerThunk } from "../services/auth-thunks";
-
 function RegisterScreen() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    const [role, setRole] = useState("");
+
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const handleRegister = async () => {
         try {
-           await dispatch(registerThunk({ username, password, firstName, lastName }));
+            await dispatch(registerThunk({ username, password, firstName, lastName, role, email , phone, address }));
             navigate("/youboxd/profile");
         } catch (e) {
             alert(e);
@@ -20,7 +24,7 @@ function RegisterScreen() {
     };
     return (
         <div>
-            <h1>Register Screen</h1>
+            <h1>Register</h1>
             <div className="mt-2">
                 <label>Username</label>
                 <input className="form-control" type="text" value={username}
@@ -32,20 +36,57 @@ function RegisterScreen() {
                     onChange={(event) => setPassword(event.target.value)} />
             </div>
             <div className="mt-2">
-                <label>First Name</label>
+                <label>FirstName</label>
                 <input className="form-control" type="text" value={firstName}
                     onChange={(event) => setFirstName(event.target.value)} />
             </div>
+
             <div className="mt-2">
-                <label>Last Name</label>
+                <label>LastName</label>
                 <input className="form-control" type="text" value={lastName}
                     onChange={(event) => setLastName(event.target.value)} />
             </div>
+
+
+            <div className="mt-2">
+                <label>Email</label>
+                <input className="form-control" type="text" value={email}
+                    onChange={(event) => setEmail(event.target.value)} />
+            </div>
+
+            <div className="mt-2">
+                <label>Phone</label>
+                <input className="form-control" type="text" value={phone}
+                    onChange={(event) => setPhone(event.target.value)} />
+            </div>
+
+            <div className="mt-2">
+                <label>Address</label>
+                <input className="form-control" type="text" value={avatar}
+                    onChange={(event) => setAddress(event.target.value)} />
+            </div>
+
+
+             <div className="mt-2">
+                <label>Choose Your Role.</label>
+                <select type="text" value={role} onChange={ (event) => setRole(event.target.value)} > 
+                <option >
+                        Viewer
+                    </option>
+                <option>
+                        Administrator
+                    </option>
+                    <option >
+                        User
+                    </option>
+                </select>
+            </div> 
             <button className="btn btn-primary mt-2"
                 onClick={handleRegister}>
                 Register
             </button>
         </div>
     );
+
 }
 export default RegisterScreen;
